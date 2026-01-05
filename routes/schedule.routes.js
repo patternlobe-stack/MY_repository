fd2754e12b587570ee63ca3ccb5498f59f64aa30
unknown <patterlobe@gmail.com>
2026-01-05 16:06:26 +0900

    fix route order before listen

--- routes/schedule.routes.js
+++ routes/schedule.routes.js
@@ -4,4 +4,8 @@ const controller = require("../controllers/schedule.controller");
 
 router.get("/month", controller.getMonthlySchedule);
 
+router.get("/test", (req, res) => {
+  res.send("SCHEDULE ROUTE OK");
+});
+
 module.exports = router;
